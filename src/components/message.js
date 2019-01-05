1123cb55bc9aa96201a58f4d4d82240f8b60986b
CharlotteMD <charlottemdavies@hotmail.co.uk>
2019-01-05 11:41:29 +0000

    added present page

--- src/components/message.js
+++ src/components/message.js
@@ -5,10 +5,19 @@ class Message extends Component {
     render() {
         return (
             <div className="Message">
-               <h2>Happy Birthday love from Charly and Jav xxx</h2>
+               <div className="msg1">
+                    <h2>
+                        Happy Birthday Mummy!<br/>I hope you have a lovely cruise - we can't wait to hear all about it when you get back!<br/>Can't wait to celebrate your birthday with you!<br/>Love you lots and lots, love from Charly xxxxx
+                    </h2>
+                </div>
+                <div className="msg2">
+                    <h2>
+                        Happy birthday Helen!<br/>Hope you have a great time celebrating, but don't go overboard!<br/>Love from Jav xxx
+                    </h2>
+                </div>
 
-                <a href="/3" alt="next page">
-                    <img src="./images/arrow-lights.gif" alt="Next" />
+                <a className="arrow2" href="/3" alt="next page">
+                    <img className="arrowButton2" src="./images/arrow-lights.gif" alt="Next" />
                 </a>
             </div>
         );
--- src/components/surprise.js
+++ src/components/surprise.js
@@ -7,8 +7,10 @@ class Surprise extends Component {
             <div className="Surprise">
                 <h2>Here's your present!</h2>
 
-                <a href="/4" alt="next page">
-                    <img src="./images/arrow-lights.gif" alt="Next" />
+                <iframe src="https://giphy.com/embed/4KZfqOFo63qp2" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+
+                <a className="arrow3" href="/4" alt="next page">
+                    <img className="arrowButton3" src="./images/arrow-lights.gif" alt="Next" />
                 </a>
             </div>
         );
